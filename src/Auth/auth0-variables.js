c8d09b543d26114d877a52e98910b71472b76308
AlexGz9851 <A00820225@itesm.mx>
2018-05-12 19:35:11 -0500

    changing callback
    cc

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
     domain: 'alex98510.auth0.com',
     clientId: '4Erfb6FoUkiDR5XbNfzeVdilTUCAcgbV',
-    callbackUrl: 'http://localhost:3000/callback'
+    callbackUrl: 'http://localhost:3000/callback,http://nifty-shirley-176efc.netlify.com/callback'
   }
   
